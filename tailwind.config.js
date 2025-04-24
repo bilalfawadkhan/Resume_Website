fa40a9a09ce26a00f93d93826e23ffa32085b2c1
Bilal Fawad Khan <47599282+bilalfawadkhan@users.noreply.github.com>
2025-04-24 14:09:10 +0100

    Major Update
    Project box : added hover , img corrected and text arranged.
    
    Script added to package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customPink: '#F7CFD8', // Add your custom color
+        customPinkDark:'#f1aaba', // Add your custom color
+        customYellow: `#F4F8D3`, // Add your custom color
+        customYellowDark: '#f0f5c2', // Add your custom color
+        customTeal: `#A6D6D6`, // Add your custom color
+        customTealDark: '#bbe0e0', // Add your custom color
+        csutomPurple: `#8E7DBE`, // Add your custom color
+        csutomPurpleDark: '#8471b8', // Add your custom color
+      },
+    },
   },
   plugins: [],
 }
