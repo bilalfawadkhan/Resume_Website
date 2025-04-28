55f3b29274c1ba44f75b3a611a506ae6b2293c90
Bilal Fawad Khan <47599282+bilalfawadkhan@users.noreply.github.com>
2025-04-28 15:35:03 +0100

    Box (img , color and gap) changes
    Added Progress text , theme colour of box modified to better colour composition

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
       colors: {
         customPink: '#F7CFD8', // Add your custom color
         customPinkDark:'#f1aaba', // Add your custom color
-        customYellow: `#F4F8D3`, // Add your custom color
-        customYellowDark: '#f0f5c2', // Add your custom color
-        customTeal: `#A6D6D6`, // Add your custom color
-        customTealDark: '#bbe0e0', // Add your custom color
-        csutomPurple: `#8E7DBE`, // Add your custom color
-        csutomPurpleDark: '#8471b8', // Add your custom color
+        customYellow: `#9EC6F3`, // Add your custom color
+        customYellowDark: '#579cea', // Add your custom color
+        customTeal: `#87c8c8`, // Add your custom color
+        customTealDark: '#58b2b2', // Add your custom color
+        customPurple: `#8E7DBE`, // Add your custom color
+        customPurpleDark: '#8471b8', // Add your custom color
       },
     },
   },
